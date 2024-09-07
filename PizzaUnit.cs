7c66aae2803ab587f4975ce1302d9db93ecde936
Daniil <blazeman2121@gmail.com>
2024-09-08 00:05:50 +0300

    initial test commit

---
+++ PizzaUnit.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PizzaMarketService.Controllers;
+using PizzaMarketService.Models;
+using PizzaMarketService.Repositories.IPizzaShopRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaMarketUnitService
+{
+	[TestClass]
+	public class PizzaUnit
+	{	
+		private Mock<IPizzaRepository>? _repositoryMock;
+		private PizzaController? _controllerMock;
+
+		[TestInitialize]
+		public void SetUp() 
+		{	
+			_repositoryMock = new Mock<IPizzaRepository>();
+
+			_repositoryMock.Setup(r => r.GetPizzas()).ReturnsAsync(new List<Pizza>
+			{
+				new Pizza { Id = 1, CategoryId = 1, Description = "ddddds", Name = "ssssss", },
+				new Pizza { Id = 2, CategoryId = 2, Description = "dddddssss", Name = "ssssssddddd", }
+			});
+
+			_controllerMock = new PizzaController(_repositoryMock.Object);
+
+		}
+
+		[TestMethod]
+		public async Task TestGetPizzasReturnsList()
+		{
+			var result = await _controllerMock?.GetPizzasAsync();
+
+			Assert.IsNotNull(result, "Result should not be null.");
+
+			Assert.IsTrue(result.Count > 0, "nice");
+		}
+
+
+		[TestCleanup]
+		public void Cleanup()
+		{
+
+		}
+	}
+}
