4458bfb4ce5479598274b4b44e5515cecdb67412
destructive/~# there's no way of knowing but the blood keeps flowing <69975772+shinadsu@users.noreply.github.com>
2024-09-12 22:52:17 -0700

    Update User.cs
    Should change the code

--- Models/User.cs
+++ Models/User.cs
@@ -5,11 +5,11 @@ namespace PizzaMarketService.Models
 {
 	public class User
 	{
-		public int Id						{ get; set; }
-		public string? Name					{ get; set; }
+		public int Id					{ get; set; }
+		public string? Name				{ get; set; }
 		public string? Email				{ get; set; }
-		public string? PasswordHash			{ get; set; }
-		public string? Role					{ get; set; }
+		public byte[] PasswordHash			{ get; set; } = new byte[32];
+	        public string? Role	                        { get; set; }
 		public string? Address				{ get; set; }
 		public string? PhoneNumber			{ get; set; }
 	}
